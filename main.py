6a8624eeef293b9f18753626510fd8603d42673b
Hariomruhela <it21.hariomruhela@svceindore.ac.in>
2026-03-02 23:31:11 +0530

    test coment add

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ app = FastAPI(
     version="1.0.0"
 )
 
-
+# this coment for test purpose
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
+    allow_origins=["http://localhost:3000", 
         "http://127.0.0.1:3000",],      # or ["*"] for development
     allow_credentials=True,
     allow_methods=["*"],
