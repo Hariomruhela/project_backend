a0e2f6d7d9bbdaae87745604c657093238a825d0
Hariomruhela <it21.hariomruhela@svceindore.ac.in>
2026-03-09 18:31:53 +0530

    updated model according to mysql database

--- models.py
+++ models.py
@@ -10,9 +10,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(150), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
     is_admin = Column(Boolean, default=False)
 
 
@@ -23,9 +23,9 @@ class Project(Base):
     __tablename__ = "projects"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String(200), nullable=False)
     description = Column(Text, nullable=False)
-    image_url = Column(String, nullable=False)
-    techstack = Column(String, nullable=False)  # store as comma separated string
-    live_link = Column(String, nullable=True)
+    image_url = Column(String(255), nullable=False)
+    techstack = Column(String(255), nullable=False)  # comma separated
+    live_link = Column(String(255), nullable=True)
     is_visible = Column(Boolean, default=True)
