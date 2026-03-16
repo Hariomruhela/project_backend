2977080e50e84e0e672f7f8d7e59fb9b29e88c47
Hariomruhela <it21.hariomruhela@svceindore.ac.in>
2026-03-16 15:22:30 +0530

    create admin route for manging user

---
+++ routes/admin.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import models
+from database import get_db
+from dependencies import get_current_admin
+
+router = APIRouter(
+    prefix="/api/admin",
+    tags=["Admin"]
+)
+
+# -------------------------
+# Get All Users
+# -------------------------
+@router.get("/users")
+def get_users(
+    db: Session = Depends(get_db),
+    admin = Depends(get_current_admin)
+):
+    users = db.query(models.User).all()
+    return users
+
+
+# -------------------------
+# Toggle Admin Role
+# -------------------------
+@router.put("/users/{user_id}/toggle-role")
+def toggle_role(
+    user_id: int,
+    db: Session = Depends(get_db),
+    admin = Depends(get_current_admin)
+):
+
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # prevent admin changing themselves
+    if user.id == admin.id:
+        raise HTTPException(status_code=400, detail="You cannot change your own role")
+
+    user.is_admin = not user.is_admin
+
+    db.commit()
+    db.refresh(user)
+
+    return {
+        "message": "Role updated",
+        "is_admin": user.is_admin
+    }
