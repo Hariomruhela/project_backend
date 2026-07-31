69fe2bf2df82c0b6feef8d2d0fd0f34e0eb27e8c
Hariomruhela <it21.hariomruhela@svceindore.ac.in>
2026-07-31 15:51:55 +0530

    add the frontend production url

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ origins = [
     "http://localhost:5173",
 
     # Frontend production URL
-     "https://techquitoes.vercel.app"
+     "https://techquitoes.vercel.app",
+     "https://techquitoes-protfolio.vercel.app"
 ]
 
 app.add_middleware(
